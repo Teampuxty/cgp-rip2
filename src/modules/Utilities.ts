b1a1fb880aea964624b3170d6b7cac652d653005
Teampuxty <103115535+Teampuxty@users.noreply.github.com>
2025-08-27 18:07:11 +0100

    Update Utilities.ts

--- src/modules/Utilities.ts
+++ src/modules/Utilities.ts
@@ -1,45 +1,32 @@
-// Dependencies
 import chalk from "chalk"
 import * as fs from "fs"
 
-//
 export function CreateFolder(Name: string) {
-    if (!fs.existsSync(Name))
-        fs.mkdirSync(Name)
+  if (!fs.existsSync(Name)) {
+    fs.mkdirSync(Name, { recursive: true })
+  }
 }
 
-// Creates the html file by formatting the template
 const pageTemplate = fs.readFileSync("page.html", "utf-8")
-export function FormatPageTemplate(Height: string, Width: string, ImagePath: string, VectorPath: string | undefined) {
-    const result = pageTemplate.toString().replace("HEIGHT", Height).replace("WIDTH", Width).replace("IMAGEPATH", ImagePath)
-    
-    if (VectorPath) {
-        return result.replace("VECTORPATH", VectorPath)
-    } else {
-        return result.replace("VECTORPATH", "")
-    }
+export function FormatPageTemplate(Height: string, Width: string, ImagePath: string, VectorPath?: string) {
+  let result = pageTemplate.replace("HEIGHT", Height).replace("WIDTH", Width).replace("IMAGEPATH", ImagePath)
+  return result.replace("VECTORPATH", VectorPath || "")
 }
 
-//
 export type LogType = "Success" | "Error" | "Warn" | "Neutral" | "Info"
 export const LogColourChalk = {
-    Success: chalk.green,
-    Error: chalk.red,
-    Warn: chalk.yellow,
-    Neutral: chalk.white,
-    Info: chalk.blue
+  Success: chalk.green,
+  Error: chalk.red,
+  Warn: chalk.yellow,
+  Neutral: chalk.white,
+  Info: chalk.blue
 }
-export function VerboseLog(Verbose: boolean, Type: LogType, ...args: any) {
-    // Make sure is verbose
-    if (!Verbose)
-        return
 
-    // Get the time
-    const Now = new Date()
-    const Time = `${Now.getHours().toString().padStart(2, "0")}:${Now.getMinutes().toString().padStart(2, "0")}:${Now.getSeconds().toString().padStart(2, "0")}`
-    const ColouredTime = chalk.magenta("[") + chalk.bold(Time) + chalk.magenta("]")
-
-    //
-    const ChalkColour = LogColourChalk[Type]
-    console.log(ColouredTime + " " + ChalkColour(...args))
+export function VerboseLog(Verbose: boolean, Type: LogType, ...args: any[]) {
+  if (!Verbose) return
+  const Now = new Date()
+  const Time = `${Now.getHours().toString().padStart(2, "0")}:${Now.getMinutes().toString().padStart(2, "0")}:${Now.getSeconds().toString().padStart(2, "0")}`
+  const ColouredTime = chalk.magenta("[") + chalk.bold(Time) + chalk.magenta("]")
+  const ChalkColour = LogColourChalk[Type]
+  console.log(ColouredTime + " " + ChalkColour(...args))
 }
